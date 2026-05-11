aeecad015fef88422e57d3f48eec0cc17737bd4a
Pranay <pranaytejais7@gmail.com>
2026-05-11 19:24:55 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let hamburger = document.querySelector("#headerHamburger");
+let hamMenu = document.querySelector("#hamMenu");
+let hamClose = document.querySelector("#hamClose");
+hamburger.addEventListener("click", () => {
+    hamMenu.classList.toggle("hamMenuTransition");
+})
+
+hamClose.addEventListener("click", () => {
+    hamMenu.classList.toggle("hamMenuTransition");
+})
